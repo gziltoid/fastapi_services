48f7c86c7d014fd3dade55a3c147f173a4b13880
gziltoid <g.ziltoid@gmail.com>
2021-10-05 00:00:09 +0200

    Add phone formatting

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ app = FastAPI()
 
 
 @app.post('/unify_phone_from_json')
-def unify_phone_from_json(phone: Phone):
-    return phone.number
+def unify_phone_from_json(phone: str):
+    return format_phone(phone)
+
+
+def format_phone(phone):
+    phone_digits = ''.join(ch for ch in phone if ch.isdigit())
+
+    if len(phone_digits) not in [10, 11] \
+            or phone_digits[0] not in '789' \
+            or phone_digits[-10] != '9':
+        return phone_digits
+
+    return '8 (9{}{}) {}{}{}-{}{}-{}{}'.format(*phone_digits[-9:])
